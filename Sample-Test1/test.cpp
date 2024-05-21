f5fb6a8b58ef444f540377ad20254b6900e94620
iamyunsu <iamyunsu@naver.com>
2024-05-21 09:24:29 +0900

    first commit

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,26 @@
+﻿#include "gtest/gtest.h"
+#include "../teamwork/Calc.cpp"
+
+TEST(calc, calcSum) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(calc, calcGop) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(calc, calcZegop) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(calc, calcMinus) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(calc, calcDivide) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(calc, calcSumSum) {
+	EXPECT_EQ(1, 1);
+}
