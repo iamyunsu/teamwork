cfaf823c811087c8de5f95e9cd274ccb6ef02501
hitn8757@gmail.com <hitn8752@gmail.com>
2024-05-21 09:39:35 +0900

    add getZegop
    Signed-off-by: ihchoi95@gmail.com <ihchoi95@gmail.com>

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -10,7 +10,10 @@ TEST(calc, calcGop) {
 }
 
 TEST(calc, calcZegop) {
-	EXPECT_EQ(1, 1);
+	Calc calc;
+	EXPECT_EQ(4, calc.getZegop(2));
+	EXPECT_EQ(25, calc.getZegop(5));
+	EXPECT_EQ(100, calc.getZegop(10));
 }
 
 TEST(calc, calcMinus) {
